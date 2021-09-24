675571f999fccdedb482e58043efc413f7512aa7
SergeiMeza <sergeimeza@gmail.com>
2021-09-25 04:10:23 +0900

    edit hook

--- lib/hooks/foundation/useAsync.d.ts
+++ lib/hooks/foundation/useAsync.d.ts
@@ -1,6 +1,9 @@
+/// <reference types="react" />
 declare const useAsync: (callback: () => Promise<any>, dependencies?: any[]) => {
     loading: boolean;
     error: any;
     value: any;
+    setError: import("react").Dispatch<any>;
+    setValue: import("react").Dispatch<any>;
 };
 export default useAsync;
