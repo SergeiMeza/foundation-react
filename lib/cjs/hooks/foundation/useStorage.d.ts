017370cd01747fbdd191775f7f0e1cccf4836971
SergeiMeza <sergeimeza@gmail.com>
2021-12-16 11:35:39 +0900

    add typing

--- lib/cjs/hooks/foundation/useStorage.d.ts
+++ lib/cjs/hooks/foundation/useStorage.d.ts
@@ -1,2 +1,2 @@
-export declare const useLocalStorage: (key: string, defaultValue: any) => any[];
-export declare const useSessionStorage: (key: string, defaultValue: any) => any[];
+export declare const useLocalStorage: (key: string, defaultValue: any) => readonly [any, React.Dispatch<any>, () => void];
+export declare const useSessionStorage: (key: string, defaultValue: any) => readonly [any, React.Dispatch<any>, () => void];
--- src/lib/hooks/foundation/useStorage.ts
+++ src/lib/hooks/foundation/useStorage.ts
@@ -1,14 +1,24 @@
 import { useCallback, useState, useEffect } from 'react'
 
-export const useLocalStorage = (key: string, defaultValue: any) => {
+export const useLocalStorage = (
+  key: string,
+  defaultValue: any,
+): readonly [any, React.Dispatch<any>, () => void] => {
   return useStorage(key, defaultValue, window.localStorage)
 }
 
-export const useSessionStorage = (key: string, defaultValue: any) => {
+export const useSessionStorage = (
+  key: string,
+  defaultValue: any,
+): readonly [any, React.Dispatch<any>, () => void] => {
   return useStorage(key, defaultValue, window.sessionStorage)
 }
 
-const useStorage = (key, defaultValue, storageObject) => {
+const useStorage = (
+  key,
+  defaultValue,
+  storageObject,
+): readonly [any, React.Dispatch<any>, () => void] => {
   const [value, setValue] = useState(() => {
     const jsonValue = storageObject.getItem(key)
     if (jsonValue != null) return JSON.parse(jsonValue)
@@ -29,5 +39,5 @@ const useStorage = (key, defaultValue, storageObject) => {
     setValue(undefined)
   }, [])
 
-  return [value, setValue, remove]
+  return [value, setValue, remove] as const
 }
